cb9e66592382133a01e9884e6adc10cda2b80c79
MegaGastPvP <30814097+MegaGastPvP@users.noreply.github.com>
2017-12-08 19:59:11 -0500

    Update Skeleton.php

--- src/pocketmine/entity/Skeleton.php
+++ src/pocketmine/entity/Skeleton.php
@@ -1,1 +1,55 @@
+<?php
 
+declare(strict_types=1);
+
+namespace pocketmine\entity;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\item\Item as ItemItem;
+use pocketmine\item\ItemFactory;
+use pocketmine\Player;
+
+class Skeleton extends Monster{
+
+	const NETWORK_ID = self::SKELETON;
+
+
+
+	public $width = 1;
+
+	public $height = 2;
+
+
+
+	public function getName() : string{
+
+		return "Skeleton";
+
+	}
+
+
+
+	public function getDrops() : array{
+
+		$drops = [
+
+			ItemFactory::get(ItemItem::BONE, 0, mt_rand(0, 2))
+
+		];
+		if(mt_rand(0, 199) < 5){
+			switch(mt_rand(0, 2)){
+				case 0:
+					$drops[] = ItemFactory::get(ItemItem::IRON_INGOT, 0, 1);
+					break;
+				case 1:
+					$drops[] = ItemFactory::get(ItemItem::CARROT, 0, 1);
+					break;
+				case 2:
+					$drops[] = ItemFactory::get(ItemItem::POTATO, 0, 1);
+					break;
+			}
+		}
+
+		return $drops;
+	}
+}
